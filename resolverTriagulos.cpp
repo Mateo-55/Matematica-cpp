86ae887fe9e178b08d972bdbf6c14b240c51da6f
Mateo-55 <mateobaol2010@gmail.com>
2026-05-11 22:25:19 +0000

    Resolución de triangulos rectangulos y oblicuangulos

--- resolverTriagulos.cpp
+++ resolverTriagulos.cpp
@@ -1,5 +1,16 @@
+// Resolución de triangulos oblicangulos y rectangulos
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 long double A=0, B=0, C=0, a=0, b=0, c=0;
+
+// La biblioteca <cmath> trabaja con radianes \
+   pero para este proyecto optaremos por usar \
+   usar el sistema sexagesimal
+   
+// Creación de funciones que travajen con el sistema sexagesimal
+
+
+
+int main() {}
