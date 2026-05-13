2df9e7718062d5fb583234f852d4610ddacfeabb
Mateo-55 <mateobaol2010@gmail.com>
2026-05-13 00:26:49 +0000

    mayor robustes a la función resolverTriangulo() y correccion de errores fatales

--- resolverTriagulos.cpp
+++ resolverTriagulos.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-using namespace std;
+using namespace std; // uso de namespace std para facilitar la lectura y escritura del codigo
 
-long double A=0, B=0, C=0, a=0, b=0, c=0;
+// Declaracion de variables globales para ahorrar memoria
+long double A=0, B=0, C=0, a=0, b=0, c=0; // Uso de long double para mayor presición
 
-// La biblioteca <cmath> trabaja con radianes \
-   pero para este proyecto optaremos por usar \
+/*
+   La biblioteca <cmath> trabaja con radianes 
+   pero para este proyecto optaremos por usar 
    usar el sistema sexagesimal
+*/
 
-// Creación de funciones que travajen con el sistema sexagesimal
-
+// Creación de funciones que trabajen con el sistema sexagesimal
 long double arc_sen(long double A) { return asin(A) * 180/M_PI; } // arco seno
 long double arc_cos(long double A) { return acos(A) * 180/M_PI; } // arco coseno
 long double arc_tan(long double A) { return atan(A) * 180/M_PI; } // arco tangente
@@ -21,15 +23,15 @@ long double _cos(long double A) { return cos(A * M_PI/180); } // coseno
 long double _tan(long double A) { return tan(A * M_PI/180); } // tangente
 
 // Procesos para solucionar triangulos
-void resolverTriangulo();
-void casoLLA();
-void casoLAA();
-void casoLLL();
+void resolverTriangulo(); // saber si el triangulo tiene soucion
+void casoLLA(); // Caso lado lado angulo
+void casoLAA(); // Caso lado angulo angulo
+void casoLLL(); // Caso lado lado lado
 
 
 
 int main() {
-    string azul = "\033[44m", resetColor = "\033[0m";
+    string azul = "\033[44m", resetColor = "\033[0m"; // colores para el fondo
 
     // letrero
     cout<< endl
@@ -38,6 +40,7 @@ int main() {
         << "\t" << azul << "---------------------------------------------" << endl << resetColor
         << endl ;
     
+    // input
     cout << "Ingrese los datos (si el dato es desconocido ingrese 0)\n? ";
     cin >> A >> B >> C >> a >> b >> c;
 
@@ -51,29 +54,41 @@ int main() {
         << resetiosflags(ios::left) << endl;
 
     cout << "   Razones de A" << endl;
-    cout << "Seno: " << _sen(A) << "º \tCoseno: " << _cos(A) << "º\t Tangente: " << _tan(A) << endl << endl;
+    cout << "Seno: " << _sen(A) << " \tCoseno: " << _cos(A) << "\t Tangente: " << _tan(A) << endl << endl;
 
     cout << "   Razones de B" << endl;
-    cout << "Seno: " << _sen(B) << "º \tCoseno: " << _cos(B) << "º\t Tangente: " << _tan(B) << endl << endl;
+    cout << "Seno: " << _sen(B) << " \tCoseno: " << _cos(B) << "\t Tangente: " << _tan(B) << endl << endl;
     
     cout << "   Razones de C" << endl;
-    cout << "Seno: " << _sen(C) << "º \tCoseno: " << _cos(C) << "º\t Tangente: " << _tan(C) << endl << endl;
+    cout << "Seno: " << _sen(C) << " \tCoseno: " << _cos(C) << "\t Tangente: " << _tan(C) << endl << endl;
 
     return 0;
 }
 
 void resolverTriangulo() {
-    int cantLados=0, cantAngulos=0;
-    if (A!=0) cantAngulos++;
-    if (B!=0) cantAngulos++;
-    if (C!=0) cantAngulos++;
-    if (a!=0) cantLados++;
-    if (b!=0) cantLados++;
-    if (c!=0) cantLados++;
+    int cantLados=0, cantAngulos=0, cantDatos=0;
 
-    if (cantLados == 3) casoLLL();
-    if (cantLados == 2 && cantAngulos == 1) casoLLA();
-    if (cantLados == 1 && cantAngulos == 2) casoLAA();
+    // asegurarse de que exista solucion
+    if ( A>180 || B>180 || C>180 || a>c+b || b>a+c || c>a+b || a<abs(c-b) || b<abs(a-c) || c<abs(a-b) ||
+         A < 0 || B < 0 || C < 0 || a < 0 || b < 0 || c < 0 )  {
+        cerr << "No se puede solucionar." << endl;
+        exit(1);
+    } else if  (a==0 && b==0 && c==0) {
+        cerr << "No se puede solucionar." << endl;
+        exit(1);
+    }
+
+    if (A!=0) { cantAngulos++; cantDatos++; }
+    if (B!=0) { cantAngulos++; cantDatos++; }
+    if (C!=0) { cantAngulos++; cantDatos++; }
+    if (a!=0) { cantLados++; cantDatos++; }
+    if (b!=0) { cantLados++; cantDatos++; }
+    if (c!=0) { cantLados++; cantDatos++; }
+
+    if (cantLados == 3) { casoLLL(); } else
+    if (cantLados == 2 && cantAngulos) { casoLLA(); } else
+    if (cantLados && cantAngulos == 2) { casoLAA(); } else 
+    { cerr << "No se puede solucionar." << endl; exit(1); }
 
 }
 
@@ -87,16 +102,16 @@ void casoLLL() {
 void casoLLA() {
     if (a != 0) {
         if (b!=0) {
-            if (B != 0) { // B a b
+            if (B != 0) { // caso B a b
                 A = arc_sen((a*_sen(B))/b);
                 C = 180.0L - (A + B);
                 c = (b*_sen(C))/_sen(B);
 
-            } else if (A != 0) { // A a b
+            } else if (A != 0) { // caso A a b
                 B = arc_sen((b*_sen(A))/a);
                 C = 180.0L - (A + B);
                 c = (b*_sen(C))/_sen(B);
-            } else { // C a b
+            } else { // caso C a b
                 c = sqrt( (a*a)+(b*b)-2*a*b*_cos(C) );
                 B = arc_sen((b*_sen(C))/c);
                 A = arc_sen((a*_sen(C))/c);
@@ -104,17 +119,17 @@ void casoLLA() {
             } 
 
         } else {
-            if (A != 0) { // A a c
+            if (A != 0) { // caso A a c
                 C = arc_sen((c*_sen(A))/a);
                 B = 180.0L - (A + C);
                 b = (a*_sen(B))/_sen(A);
 
-            } else if (B != 0) { // B a c
+            } else if (B != 0) { // caso B a c
                 b = sqrt( (a*a)+(c*c)-2*a*c*_cos(B) );
                 A = arc_sen((a*_sen(B))/b);
                 C = 180.0L - (A + B);
 
-            } else { // C a c
+            } else { // caso C a c
                 A = arc_sen((a*_sen(C))/c);
                 B = 180.0L - (A + C);
                 b = (c*_sen(B))/_sen(C);
@@ -124,15 +139,15 @@ void casoLLA() {
         }
         
     } else {
-        if (A != 0) { // A b c
+        if (A != 0) { // caso A b c
             a = sqrt(b*b + c*c - 2 * b * c * _cos(A));
             C = arc_sen((c*_sen(A))/a);
             B = arc_sen((b*_sen(A))/a);
-        } else if (B != 0) {  // B b c
+        } else if (B != 0) {  // caso B b c
             C = arc_sen((c*_sen(B))/b);
             A = 180.0L - (B + C);
             a = b*_sen(A)/_sen(B);
-        } else {  // C b c
+        } else {  // caso C b c
             B = arc_sen((b*_sen(C))/c);
             A = 180.0L - (B + C);
             a = c*_sen(A)/_sen(C);
@@ -143,38 +158,38 @@ void casoLLA() {
 void casoLAA() {
 
     if (A!= 0) {
-        if (a != 0) { // A C a
+        if (a != 0) { // caso A C a
             B = 180 - C - A;
-            b = a*_tan(B);
-            c = a/_cos(B);
-        } else if (b != 0) { // A C b
+            b = a*_sen(B)/_sen(A);
+            c = a*_sen(C)/_sen(A);
+        } else if (b != 0) { // caso A C b
             B = 180 - C - A;
-            a = b*_tan(A);
-            c = b/_cos(A);
+            a = b*_sen(A)/_sen(B);
+            c = b*_sen(C)/_sen(B);
 
-        } else if (c != 0) { // A C c
+        } else if (c != 0) { // caso A C c
             B = 180 - C - A;
-            a = c*_cos(A);
-            b = c*_cos(B);
+            a = c*_sen(A)/_sen(C);
+            b = c*_sen(B)/_sen(C);
         }
     } else if (B!=0)
     {
-        if (a != 0) { // B C a
+        if (a != 0) { // caso B C a
             A = 180 - B - C;
-            b = a*_tan(B);
-            c = a/_cos(B);
+            b = a*_sen(B)/_sen(A);
+            c = a*_sen(C)/_sen(A);
 
-        } else if (b != 0) { // B C b
+        } else if (b != 0) { // caso B C b
             A = 180 - B - C;
-            a = b*_tan(A);
-            c = b/_sen(B);
-        } else if (c != 0) { // B C c
+            a = b*_sen(A)/_sen(B);
+            c = b*_sen(C)/_sen(B);
+        } else if (c != 0) { // caso B C c
             A = 180 - B - C;
-            b = c/_sen(B);
-            a = b*_tan(A);
+            b = c*_sen(B)/_sen(C);
+            a = c*_sen(A)/_sen(C);
         }
     }
 
 }
 
-// Desarrollado en su totalidad por Mateo B.
+// Desarrollado por Mateo B.
