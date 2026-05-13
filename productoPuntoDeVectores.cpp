e27229477f6dd01eff21580fdeb3a5138f68fbc6
Mateo-55 <mateobaol2010@gmail.com>
2026-05-13 04:22:58 +0000

    Proyecto terminado

--- productoPuntoDeVectores.cpp
+++ productoPuntoDeVectores.cpp
@@ -13,7 +13,30 @@ int main() {
         << "\t" << azul << "==============================================" << endl << resetColor
         << endl ;
     
-    cout << "Ingrese la longitud de los vectores\n? ";
+    // input
+    cout << "\tIngrese la longitud de los vectores\n? ";
+    int cantDatos;
+    cin >> cantDatos;
+
+    vector<double> vec1(cantDatos), vec2(cantDatos);
+
+    cout << "\tIngrese los vectores\n";
+    for (int i = 0; i < cantDatos; i++)
+        cin >> vec1[i];
+    
+    for (int i = 0; i < cantDatos; i++)
+        cin >> vec2[i];
+
+    // output
+    double producto=0;
+    for (int i = 0; i < cantDatos; i++)
+        producto += vec1[i] * vec2[i];
+    
+    if ((int)producto == producto) {
+        cout << "\n   El producto punto es: " << azul << producto << resetColor << endl;
+    } else {
+        cout << "\n   El producto punto es: " << azul << setprecision(5) << fixed << producto << resetColor << endl;
+    }
     
     return 0;
 }
