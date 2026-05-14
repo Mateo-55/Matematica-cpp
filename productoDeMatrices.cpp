1307a93cfae2e77ade8af15adbff14821f6a8459
Mateo-55 <mateobaol2010@gmail.com>
2026-05-14 04:09:55 +0000

    cree el letrero

--- productoDeMatrices.cpp
+++ productoDeMatrices.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 using namespace std;
 
+double productoPunto(vector<double>vec1, vector<double> vec2, int tam) {
+
+}
+
 int main () {
+     string azul = "\033[44m", resetColor = "\033[0m"; // colores para el fondo
+
+    // letrero
+    cout<< endl
+        << "\t" << azul << "==============================================" << endl << resetColor
+        << "\t" << azul << "============ Producto De Matrizes ============" << endl << resetColor
+        << "\t" << azul << "==============================================" << endl << resetColor
+        << endl;
+
     return 0;
 }
