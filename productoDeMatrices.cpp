9352fc62780e0b8cf8bd9239ba36bdebe9cffb46
MATEO BANOS OLVERA <mateobanosolvera@MacBook-Air-de-MATEO.local>
2026-05-15 12:17:36 -0600

    Proyecto terminado

--- productoDeMatrices.cpp
+++ productoDeMatrices.cpp
@@ -29,41 +29,49 @@ int main () {
 
     // Validar la multiplicacion
 
-    if (y1 != x2 && y2 != x1) {        
-        cerr << "No es posible realizar la multiplicación." << endl;
-        exit(1);
+    bool aMulB = (x1 == y2);
+    bool bMulA = (x2 == y1);
+
+    if (!aMulB && !bMulA) {
+        cerr << "No es posible realizar la multiplicación en ninguna." << endl;
+        return 1;
     }
 
     // input
-    vector<vector<int>> a(y1, vector<int>(x1));
-    vector<vector<int>> b(y2, vector<int>(x2));
+    vector<vector<double>> a(y1, vector<double>(x1));
+    vector<vector<double>> b(y2, vector<double>(x2));
 
     cout << "\tIngrese la matriz 1" << endl;
     for (int i = 0; i < y1; i++) {
         for (int j = 0; j < x1; j++) {
-            cin >> a[i][j];  
+            cin >> a[i][j];
         }
     }
     
     cout << "\tIngrese la matriz 2" << endl;
     for (int i = 0; i < y2; i++) {
         for (int j = 0; j < x2; j++) {
-            cin >> b[i][j];  
+            cin >> b[i][j];
         }
     }
     
     int n, m, o;
-    if ( y1 == x2 ) { // el eje comun es y1 x2
-        n = y2;
-        m = x1;
-        o = y1;
-    } else { // el eje comun es y2 x1
+    bool useAfirst;
+
+    if (aMulB) {
+        useAfirst = true;
         n = y1;
         m = x2;
-        o = y2;
+        o = x1;
+    } else {
+        useAfirst = false;
+        n = y2;
+        m = x1;
+        o = x2;
     }
 
-    double matrizProducto[n][m];
+    vector<vector<double>> matrizProducto(n, vector<double>(m));
+
     /*
         cuando tenemos dos matrizes
          ----------    ----------
@@ -85,19 +93,33 @@ int main () {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            vector<int> v1(o), v2(o);
-            for (int k = 0; k < o; k++) {    
-                v1[k] = a[i][k];
-                v2[k] = b[k][j];
+            vector<double> v1(o), v2(o);
+            for (int k = 0; k < o; k++) {
+                if (useAfirst) {
+                    v1[k] = a[i][k];
+                    v2[k] = b[k][j];
+                } else {
+                    v1[k] = b[i][k];
+                    v2[k] = a[k][j];
+                }
             }
+            matrizProducto[i][j] = productoPunto(v1, v2, o);
         }
     }
     
-
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) { cout << matrizProducto[i][j] << ' '; }
-        cout << endl;
+        for (int j = 0; j < m; j++) {
+            
+            if ((int) matrizProducto[i][j] == matrizProducto[i][j]) { // << setw(11) setiosflags(ios::left)
+                cout << defaultfloat << azul << matrizProducto[i][j] << resetColor << "\t\t";
+            } else {
+                cout << setprecision(5) << fixed << azul << matrizProducto[i][j] << resetColor << "\t";
+            }
+        }
+        cout << endl << endl;
     }
+    
+
 
     return 0;
 }
