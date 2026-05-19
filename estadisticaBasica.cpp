d16ed1c574a73699340e0606651bd7302a1a6641
MATEO BANOS OLVERA <mateobanosolvera@MacBook-Air-de-MATEO.local>
2026-05-19 16:55:53 -0600

    Medidas de tendencia central y de dispersion

--- estadisticaBasica.cpp
+++ estadisticaBasica.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <typeinfo>
 using namespace std;
 
+void prom(vector<double>, double &, const int);
+void desv_med(vector<double>, const int, double&, const double);
+void sum(vector<double>, const int, double&);
+void med(vector<double>, const int, double&);
 
 int main() {
      string azul = "\033[44m", resetColor = "\033[0m"; // colores para el fondo
@@ -15,5 +18,68 @@ int main() {
         << "\t" << azul << "==============================================" << endl << resetColor
         << endl ;
 
+    // Inupt
+    int cantDatos;
+    cout << "Ingrese la cantidad de datos\n? ";
+    cin >> cantDatos;
+
+    vector<double> lista(cantDatos);
+    cout << "\nIngrese los " << cantDatos << " datos\n? ";
+    for (int i = 0; i < cantDatos; i++)
+        cin >> lista[i];
+    
+    double promedio = 0, desvMedia = 0, sumatoria = 0, mediana = 0;
+
+    prom(lista, promedio, cantDatos);
+    desv_med(lista, cantDatos, desvMedia, promedio);
+    sum(lista, cantDatos, sumatoria);
+    med(lista, cantDatos, mediana);
+
+    cout<< endl 
+        << "Promedio:          " << promedio  << endl
+        << "Desviacion media:  " << desvMedia << endl
+        << "Sumatoria:         " << sumatoria << endl
+        << "Mediana:           " << mediana   << endl;
+
     return 0;
+}
+
+void prom(vector<double> arr, double &p, const int tam) {
+    for (int i=0; i<tam; i++)
+        p += arr[i];
+
+    p /= tam;
+}
+
+void desv_med(vector<double> arr, const int tam, double &desvMedia, const double p) {
+    vector<double> a(tam);
+
+    for (int i = 0; i < tam; i++)
+        a[i] = abs(arr[i] - p);
+        
+        prom(a, desvMedia, tam);
+}
+
+void sum(vector<double> arr, const int tam, double &sumatoria) {
+    for (int i = 0; i < tam; i++)
+        sumatoria += arr[i];
+}
+
+void med(vector<double> arr, const int tam, double &mediana) {
+    for (int i = 0; i < tam; i++) {
+        for (int j = 0; j < tam-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                double tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+
+    if (tam%2) {
+        mediana = arr[((tam+1)/2)-1];
+    } else {
+        mediana = ( arr[tam/2] + arr[(tam/2)-1] ) / 2;
+    }
+    
 }
