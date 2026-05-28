c59dc6baafd69b5ed873b8eb6c62167ace23376d
Mateo-55 <mateobaol2010@gmail.com>
2026-05-28 20:17:49 +0000

    añadi moda

--- estadisticaBasica.cpp
+++ estadisticaBasica.cpp
@@ -7,6 +7,7 @@ void prom(vector<double>, double &, const int);
 void desv_med(vector<double>, const int, double&, const double);
 void sum(vector<double>, const int, double&);
 void med(vector<double>, const int, double&);
+void mod(vector<double>, const int, double&);
 
 int main() {
      string azul = "\033[44m", resetColor = "\033[0m"; // colores para el fondo
@@ -28,17 +29,21 @@ int main() {
     for (int i = 0; i < cantDatos; i++)
         cin >> lista[i];
     
-    double promedio = 0, desvMedia = 0, sumatoria = 0, mediana = 0;
+    // calculo
+    double promedio = 0, desvMedia = 0, sumatoria = 0, mediana = 0, moda = 0;
 
     prom(lista, promedio, cantDatos);
     desv_med(lista, cantDatos, desvMedia, promedio);
     sum(lista, cantDatos, sumatoria);
     med(lista, cantDatos, mediana);
+    mod(lista, cantDatos, moda);
 
+    // output
     cout<< endl 
         << "Promedio:          " << promedio  << endl
         << "Desviacion media:  " << desvMedia << endl
         << "Sumatoria:         " << sumatoria << endl
+        << "Moda:              " << moda      << endl
         << "Mediana:           " << mediana   << endl;
 
     return 0;
@@ -82,4 +87,26 @@ void med(vector<double> arr, const int tam, double &mediana) {
         mediana = ( arr[tam/2] + arr[(tam/2)-1] ) / 2;
     }
     
+}
+
+void mod(vector<double> arr, const int l, double &mediana) {
+    double m[2] = {arr[0], 0};
+    for (int i = 0; i < l; i++) {
+        if (i==0) {
+            for (int j = 0; j < l; j++) {
+                if (arr[j]  == m[0]) m[1]++;
+            }
+        }
+
+        if (arr[i] != m[0]) {
+            double t[2] = {arr[i], 0};
+            for (int j = 0; j < l; j++) {
+                if (t[0] == arr[j]) t[1]++;
+            }
+
+            if (t[1] > m[1]) m[0]=t[0], m[1]=t[1];
+        }
+    }
+
+    mediana = m[0];
 }
