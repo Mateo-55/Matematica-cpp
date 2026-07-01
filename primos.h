5e6f0c141656c1e78cdd0cb633cdf59d470d17a9
MATEO BANOS OLVERA <mateobanosolvera@MacBook-Air-de-MATEO.local>
2026-06-30 22:43:09 -0600

    Añadi algunas funciones utiles

--- primos.h
+++ primos.h
@@ -4,3 +4,25 @@ bool esPrimo(int x) {
 
     return true;
 }
+
+int primoSiguente(int p) {
+    int n;
+    for (int i = p; true; i++) {
+        if (esPrimo(i)) {
+            n = i;
+            break;
+        }
+    }
+    return n;
+}
+
+int primoAnterior(int p) {
+    int n;
+    for (int i = p - 1; i >= 0; i--) {
+        if (esPrimo(i)) {
+            n=i;
+            break;
+        }
+    }
+    return n;
+}
