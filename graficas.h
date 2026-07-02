5501ac8122a5abe7771b0044a3a201580db4fff1
MATEO BANOS OLVERA <mateobanosolvera@MacBook-Air-de-MATEO.local>
2026-07-01 22:46:52 -0600

    Cree el archivo, la clase , el contructor, getters, setters, y los metodos: redimencionar, vaciarFondo y remplazar

--- graficas.h
+++ graficas.h
@@ -1,3 +1,5 @@
+#ifndef GRAFICAS_H
+#define GRAFICAS_H
 #include <vector>
 
 class grafica {
@@ -9,24 +11,84 @@ public:
     int centroX;
     int centroY;
     std::vector<std::vector<char>> fondo;
-    
-    grafica(int x, int y) {
+
+    grafica(int x, int y) { // constructor
         this->x = x;
         this->y = y;
-        centroX = x/2;
-        centroY = y/2;
-        initFondo(x, y);
+        setCentroX();
+        setCentroY();
+        initFondo();
     }
 
-    void initFondo(int x, int y) {
-        fondo.resize(y, std::vector<char>(x));
+    void initFondo() {
+        fondo.resize(y, std::vector<char>(x, ' '));
     }
-
-    void setCentroX(int n) {
+    
+    // getters
+    int getCentroX() {
+        return centroX;
+    }
+    int getCentroY() {
+        return centroY;
+    }
+    std::vector<std::vector<char>> getFondo() {
+        return fondo;
+    }
+    int getX() {
+        return x;
+    }
+    int getY() {
+        return y;
+    }
+    
+    // setters
+    void setX(int x) { // set x
+        this->x = x;
+        redimencionar(x, y);
+    }
+    void setY(int y) { // set y
+        this->y = y;
+        redimencionar(x, y);
+    }
+    void setCentroX(int n) { // set centroX con un valor de referencia
         centroX = n;
     }
-
-    void setCentroY(int n) {
+    void setCentroY(int n) { // set centroY con un valor de referencia
         centroY = n;
     }
+    void setCentroX() { // set centroX con base a x
+        centroX = x/2;
+    }
+    void setCentroY() { // set centroY con base a y
+        centroY = y/2;
+    }
+
+    void redimencionar(int x, int y) {
+        this->x = x;
+        this->y = y;
+        fondo.resize(y, std::vector<char>(x, ' '));
+        setCentroX();
+        setCentroY();
+    }
+
+
+    void vaciarFondo() {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                fondo[i][j] = ' ';
+            }
+        }
+    }
+
+    void remplazar( grafica &g ) {
+        this->x = g.x;
+        this->y = g.y;
+        this->centroX = g.centroX;
+        this->centroY = g.centroY;
+        this->fondo = g.fondo;
+    }
 };
+
+#endif
+
+// Creado por Mateo B.
