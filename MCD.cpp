d35cb6d50fa678a803f7a1110aa94e5fcc243f93
MATEO BANOS OLVERA <mateobanosolvera@MacBook-Air-de-MATEO.local>
2026-07-03 12:02:53 -0600

    Archivo creado

---
+++ MCD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "primos.h"
+using namespace std;
+
+int main() {
+    string azul = "\033[44m", resetColor = "\033[0m"; // colores para el fondo
+    // letrero
+    cout<< endl
+        << "\t" << azul << "==============================================" << endl << resetColor
+        << "\t" << azul << "============ Maximo Común Divisor ============" << endl << resetColor
+        << "\t" << azul << "==============================================" << endl << resetColor
+        << endl;
+    
+    cout << "Ingrese la cantidad de datos\n? ";
+    int n;
+    cin >> n;
+
+
+
+    vector<int>;
+    return 0;
+}
